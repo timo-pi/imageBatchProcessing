4bc5deb1906ffc61633ca75df409a463355cd8c9
timo-pi <timopiechotta@gmail.com>
2021-02-09 08:10:55 +0100

    A/V (ffmpeg) + bessere png Kompression (pngquant) eingebaut

--- Image_Batch_Processing.py
+++ Image_Batch_Processing.py
@@ -6,8 +6,11 @@ import os
 from pathlib import Path
 import shutil
 from configparser import ConfigParser
+import subprocess
+from shutil import move
+import time
 
-# pyinstaller --add-data "config.ini;." --clean -y Image_Batch_Processing.py
+# pyinstaller --add-data "config.ini;." --add-data "ffmpeg.exe;." --add-data "pngquant.exe;." --clean -y Image_Batch_Processing.py
 
 # READ CONFIG INI
 config = ConfigParser()
@@ -20,11 +23,15 @@ resize_factor = float(config.get('section_a', 'resize_factor'))
 compression = int(config.get('section_a', 'compression'))
 width = int(config.get('section_a', 'width'))
 height = int(config.get('section_a', 'height'))
-max_colors = bool(config.get('section_a', 'max_colors'))
-jpg_resize = bool(config.get('section_a', 'jpg_resize'))
-png_resize = bool(config.get('section_a', 'png_resize'))
-gif_resize = bool(config.get('section_a', 'gif_resize'))
+max_colors = config.getboolean('section_a', 'max_colors')
+jpg_resize = config.getboolean('section_a', 'jpg_resize')
+png_resize = config.getboolean('section_a', 'png_resize')
+gif_resize = config.getboolean('section_a', 'gif_resize')
+convert_av = config.getboolean('section_a', 'convert_av')
 video_bitrate = config.get('section_a', 'video_bitrate')
+audio_bitrate = config.get('section_a', 'audio_bitrate')
+png_quality = config.get('section_a', 'png_quality')
+max_png_size = int(config.get('section_a', 'max_png_size'))
 
 def extractScorm(zip_file):
     # btn_select.config(state="disabled")
@@ -40,24 +47,14 @@ def extractScorm(zip_file):
 def adjustImage(file_path, image_type, report):
     img = Image.open(file_path) #.convert('RGB')
     global width, height
-    if os.path.getsize(file_path) > min_file_size and img.size[0] >width and img.size[1] > height:
+    png_file_size = os.path.getsize(file_path)
+    if png_file_size > min_file_size and img.size[0] >width and img.size[1] > height:
         #print("Original Image Size: " + str(img.size))
-
-        """# GET ALPHA LAYER
-        red, green, blue, alpha = img.split()
-        print("alpha: " + str(alpha))"""
-
-        # RESIZE IMAGE
         if image_type == 'png':
             report.write("PNG;" + str(img.size).replace(', ', 'x') + ';')
-            global png_resize
-            if png_resize == True:
-                base = resize_factor # e.g. 1 = original size, 0.5 = half size
-                hsize = int((float(img.size[1] * base)))
-                vsize = int((float(img.size[0] * base)))
-                img = img.resize((vsize, hsize), Image.ANTIALIAS)
+
             # REDUCE COLORS
-            global max_colors
+            """global max_colors
             if img.getcolors(maxcolors=256) == None and max_colors == True:
                 print("Change color palette to 256!")
                 img = img.convert("P", palette=Image.ADAPTIVE, colors=256)
@@ -68,12 +65,33 @@ def adjustImage(file_path, image_type, report):
             #print("PNG new file size: " + str(os.path.getsize(file_path)))
             #print("PNG new resolution: " + str(img.size))
             report.write(str(os.path.getsize(file_path)) + ';')
+            report.write(str(img.size).replace(', ', 'x') + ';' + '\n') """
+
+            report.write(str(os.path.getsize(file_path)) + ';')
+            subprocess.call('pngquant --quality=' + png_quality + ' --speed 10 --ext _save.png --force ' + file_path, shell=True)
+            try:
+                move(file_path[:-4] + '_save.png', file_path)
+            except:
+                print("PNG image could not be compressed due to quality reasons!")
+
+            # REDUCE RESOLUTION
+            new_file_size = os.path.getsize(file_path)
+            if png_resize and new_file_size > max_png_size * 1000:
+                print("Resizing next image:")
+                img = Image.open(file_path)
+                base = resize_factor # e.g. 1 = original size, 0.5 = half size
+                hsize = int((float(img.size[1] * base)))
+                vsize = int((float(img.size[0] * base)))
+                img = img.resize((vsize, hsize), Image.ANTIALIAS)
+                img.save(file_path, optimize=True)
+                new_file_size = os.path.getsize(file_path)
+
+            report.write(str(new_file_size) + ';')
             report.write(str(img.size).replace(', ', 'x') + ';' + '\n')
 
         elif image_type == 'jpg':
             report.write("JPG;" + str(img.size).replace(', ', 'x') + ';')
             global jpg_resize
-            print(jpg_resize)
             if jpg_resize == True:
                 base = resize_factor # e.g. 1 = original size, 0.5 = half size
                 hsize = int((float(img.size[1] * base)))
@@ -105,6 +123,21 @@ def adjustImage(file_path, image_type, report):
         else:
             print("File size < 100 Bytes or resolution < 32x32 pixels")
 
+def adjustAudioVideo(file_path, file_type, report):
+    start_time = time.time()
+    report.write(str(os.path.getsize(file_path)) + ';')
+    if file_type == 'mp4':
+        video = subprocess.run('ffmpeg.exe -i %s -b %s %s -y' % (file_path, video_bitrate, file_path))
+        #video_libx264 = subprocess.run('ffmpeg -i %s -vcodec libx264 -b %s %s -y' % (file_path, video_bitrate, file_path))
+        report.write(str(os.path.getsize(file_path)) + ';')
+    elif file_type == 'mp3':
+        audio = subprocess.run('ffmpeg -i %s -map 0:a:0 -b:a %s %s' % (file_path, audio_bitrate, file_path))
+        report.write(str(os.path.getsize(file_path)) + ';')
+    # SCALE: -s 720x480
+    # SCALE based on input size (make it half)
+    # ffmpeg -i input.avi -vf scale="iw/2:ih/2" output.avi
+    print("--- Video converting time %s ---" % (time.time() - start_time))
+
 def checkImages(extracted_path, report):
     for root, directories, files in os.walk(extracted_path):
         for file in files:
@@ -126,11 +159,30 @@ def checkImages(extracted_path, report):
                 report.write(file_path + ';')
                 print(file_path)
                 adjustImage(file_path, "gif", report)
-            if file.endswith('.mp4') or file.endswith('.avi') or file.endswith('.mkv') or file.endswith('.mov') or file.endswith('.wmv'):
+            if file.endswith('.mp4'):
+                if convert_av:
+                    # Create the full filepath
+                    file_path = os.path.join(root, file)
+                    print(file_path)
+                    report.write(file_path + ';')
+                else:
+                    print("VIDEO DETECTED: " + str(file_path))
+                    report.write('WARNING, VIDEO FILE FOUND (mp4). File size;' + str(os.path.getsize(file_path)) + '\n')
+            if file.endswith('.mp3'):
+                if convert_av:
+                    # Create the full filepath
+                    file_path = os.path.join(root, file)
+                    print(file_path)
+                    report.write(file_path + ';')
+                    adjustAudioVideo(file_path, "mp3", report)
+                else:
+                    print("Audio DETECTED: " + str(file_path))
+                    report.write('WARNING, AUDIO FILE FOUND (mp3). File size;' + str(os.path.getsize(file_path)) + '\n')
+            if file.endswith('.avi') or file.endswith('.mkv') or file.endswith('.mov') or file.endswith('.wmv'):
                 file_path = os.path.join(root, file)
                 report.write(file_path + ';')
                 print("VIDEO DETECTED: " + str(file_path))
-                report.write('WARNING, VIDEO FILE FOUND. File size;' + str(os.path.getsize(file_path)) + '\n')
+                report.write('WARNING, VIDEO FILE FOUND (not mp4!). File size;' + str(os.path.getsize(file_path)) + '\n')
 
 def zipNewScorm(dir_name):
     print("Creating new SCORM package in directory " + str(Path(dir_name)))
